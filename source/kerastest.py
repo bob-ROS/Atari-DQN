39cc0e88c721ab16acf8d3c67baeaad8edfda624
bob-ROS <fredrik.ano@gmail.com>
2019-01-30 16:37:55 +0100

    minor changes to kerastest

--- source/kerastest.py
+++ source/kerastest.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.utils import plot_model
 import gym
+import time
 
 model = Sequential([
     Dense(32, input_shape=(784,)),
@@ -10,11 +11,17 @@ model = Sequential([
     Activation('softmax'),
 ])
 env = gym.make('BeamRider-v0')
-env.render()
+
 
 plot_model(model, to_file='model.png', show_layer_names=True, show_shapes=True)
 
 
+env.reset()
+for _ in range(1000):
+    env.render()
+    obs, reward, done, info = env.step(env.action_space.sample()) # take a random acti
+    print("Reward: %.10f" %(reward))
+    time.sleep(0.1)
 
 
 
