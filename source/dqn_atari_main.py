d04ecb8a265a9ecd38df38e9d63826c5fa85a0bc
bob-ROS <fredrik.ano@gmail.com>
2019-02-06 15:41:41 +0100

    initial image processing

--- source/dqn_atari_main.py
+++ source/dqn_atari_main.py
@@ -6,3 +6,40 @@ from keras.optimizers import Adam
 import matplotlib.pyplot as plt 
 
 
+#unprocessed_image_size = fromgame
+
+#preprocessed_image_size = x
+
+
+# return observationProcessed(imagefromstep)
+def observationProcessing(env):
+    env.reset()
+    screen = env.render(mode='rgb_array')
+    #screen = np.mean(screen,-1)
+    screen = screen.mean(-1)
+    plt.imshow(screen, cmap = plt.get_cmap('gray'))
+    plt.show()
+#initialize convolutional network
+
+
+#save / load network
+
+
+#experience_replay
+
+
+# MAIN
+
+#loop
+	#add.parser visualize or not
+
+	#blah = gym.step()
+	#obsevationProcessed(blah)
+
+	#experience_replay
+
+env = gym.make('CartPole-v0')
+observationProcessing(env)
+
+#if __name__ == "__main__":
+
